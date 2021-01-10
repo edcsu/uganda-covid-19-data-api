a5aca4cb3fa5be51dfc1e236be764d8d6df55aa2
Keith Ssewanonda <keith@laboremus.no>
2021-01-10 19:43:53 +0300

    refactor:update route list

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('students', 'ApiController@getAllStudents');
-Route::get('students/{id}', 'ApiController@getStudent');
-Route::post('students', 'ApiController@createStudent');
-Route::put('students/{id}', 'ApiController@updateStudent');
-Route::delete('students/{id}','ApiController@deleteStudent');
+Route::get('ugandaData', 'UgandaController@getAllUgandaResults');
+Route::get('ugandaData/{id}', 'UgandaController@getUgandaResult');
+Route::post('ugandaData', 'UgandaController@createUgandaResults');
+Route::put('ugandaData/{id}', 'UgandaController@updateUgandaResults');
+Route::delete('ugandaData/{id}','UgandaController@deleteUgandaResults');
